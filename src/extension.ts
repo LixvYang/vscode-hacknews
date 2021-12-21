a4f6c53d8d121ed0002abb489e953663e60a4344
lixin yang <2690688423@qq.com>
2021-12-22 00:17:46 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Output } from './global/logger';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	Output('Extension Activated')
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hacknews" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -20,7 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+	vscode.commands.registerCommand('hacknews.nihao', () => {
+		vscode.window.showInformationMessage("你好!");
+	});
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
