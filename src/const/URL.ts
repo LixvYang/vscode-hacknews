67ea52ddcfbf5ef1145afae1d15cf317441d0b4a
lixin yang <2690688423@qq.com>
2021-12-24 03:05:59 +0800

    update api

---
+++ src/const/URL.ts
@@ -0,0 +1,13 @@
+// base API for hacknews
+const BaseAPI = `https://hacker-news.firebaseio.com/v0/`;
+
+// 
+export const TopStoriesAPI = BaseAPI + `topstories.json`;
+
+export const UserAPI = BaseAPI + `user/`;
+
+export const AskstoriesAPI = BaseAPI + `askstories.json`;
+
+export const JobstoriesAPI = BaseAPI + `jobstories.json`;
+
+export const ShowstoriesAPI = BaseAPI + `showstories.json`;
---
+++ src/treeview/topStories-treeview-provider.ts
@@ -0,0 +1,3 @@
+import * as vscode from 'vscode';
+import * as httpClient from 'request';
+import { TopStoriesAPI } from '../const/URL';
